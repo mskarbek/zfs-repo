f4338d2d5107f978bb8d974c8c492ad2785bce52
Marcin Skarbek <git@skarbek.name>
2025-10-25 00:44:25 +0200

    ci: revrite check zfs

--- scripts/check_zfs.py
+++ scripts/check_zfs.py
@@ -1,22 +1,64 @@
 #!/usr/bin/env -S uv run --script
 #
 # /// script
-# dependencies = ["feedparser"]
+# dependencies = ["feedparser", "requests"]
 # ///
 import json
+import re
+from string import Template
 
 import feedparser
+import requests
 
-atom_url = "https://github.com/openzfs/zfs/releases.atom"
+atom_url = "https://github.com/openzfs/zfs/tags.atom"
 feed = feedparser.parse(atom_url)
 
-with open("openzfs.json", "w") as file:
-    file.write(
-        json.dumps(
-            {
-                "version": feed["entries"][0]["title"],
-                "url": f"https://github.com/openzfs/zfs/tags/download/{feed['entries'][0]['title']}/{feed['entries'][0]['title']}.tar.gz",
-            }
-        )
-    )
-    print(feed["entries"][0]["title"])
+tag_meta_url = Template(
+    "https://raw.githubusercontent.com/openzfs/zfs/refs/tags/${version}/META"
+)
+tag_dwonload_url = Template(
+    "https://github.com/openzfs/zfs/archive/refs/tags/${version}.tar.gz"
+)
+master_meta_url = "https://raw.githubusercontent.com/openzfs/zfs/refs/heads/master/META"
+master_dwonload_url = "https://github.com/openzfs/zfs/archive/refs/heads/master.tar.gz"
+
+releases = {
+    "2.2": {},
+    "2.3": {},
+    "2.4": {},
+    "master": {},
+}
+
+regex = re.compile(r"Linux-Maximum: +([0-9]\.[0-9][0-9])")
+
+for release in releases:
+    with open(f"versions/openzfs-{release}.json", "r+") as f:
+        if release != "master":
+            for entry in feed["entries"]:
+                if (
+                    f"zfs-{release}" in entry["title"]
+                    and ".99" not in entry["title"]
+                    and not releases[release]
+                ):
+                    version = entry["title"]
+                    releases[release]["version"] = version[4:]
+                    meta = requests.get(tag_meta_url.substitute({"version": version}))
+                    kernel_max = regex.search(meta.text)
+                    if kernel_max is not None:
+                        releases[release]["max_kernel_version"] = kernel_max.group(1)
+                    releases[release]["meta_url"] = tag_meta_url.substitute(
+                        {"version": version}
+                    )
+                    releases[release]["dwonload_url"] = tag_dwonload_url.substitute(
+                        {"version": version}
+                    )
+                    json.dump(releases[release], f, indent=2)
+        else:
+            releases[release]["version"] = "master"
+            meta = requests.get(master_meta_url)
+            kernel_max = regex.search(meta.text)
+            if kernel_max is not None:
+                releases[release]["max_kernel_version"] = kernel_max.group(1)
+            releases[release]["meta_url"] = master_meta_url
+            releases[release]["dwonload_url"] = master_dwonload_url
+            json.dump(releases[release], f, indent=2)
